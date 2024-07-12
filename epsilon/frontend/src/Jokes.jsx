702c7888e72bfe78d91cbc4fa40e3c21cb6a8764
drumilbhati <drumilbhati5@gmail.com>
2024-07-12 08:50:52 +0530

    Added temp file Jokes

---
+++ epsilon/frontend/src/Jokes.jsx
@@ -0,0 +1,33 @@
+import './HomePage.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function Jokes() {
+    const [jokes, setJokes] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/jokes')
+        .then((response) => {
+            setJokes(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    })
+
+    return (
+        <div className="jokes">
+            <h1>Jokes</h1>
+            <h2>Count: {jokes.length}</h2>
+
+            {
+                jokes.map((joke) => (
+                    <div className="joke">
+                        <h3>{joke.joke}</h3>
+                        <p>{joke.answer}</p>
+                    </div>
+                ))
+            }
+        </div>
+    );
+}
