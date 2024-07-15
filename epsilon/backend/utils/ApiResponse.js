4aaafd77bffd9449dcbfcd4f8461cd754f85a85f
drumilbhati <drumilbhati5@gmail.com>
2024-07-15 08:38:43 +0530

    Created custom API response

---
+++ epsilon/backend/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse {
+    constructor(status, data, message = "Success") {
+        this.status = status;
+        this.data = data;
+        this.message = message;
+        this.success = status < 400;
+    }
+}
--- epsilon/backend/utils/asyncHandler.js
+++ epsilon/backend/utils/asyncHandler.js
@@ -1,5 +1,8 @@
-const asyncHandler = () => {
-
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next))
+    .catch((error) => next(error));
+    }
 }
 
 export default asyncHandler;
