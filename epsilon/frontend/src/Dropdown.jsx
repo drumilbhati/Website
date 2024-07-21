a4488db1bd9f36641927cf7f82419aae28ad6964
VidhanNahar <vidhannahar@gmail.com>
2024-07-21 17:48:05 +0530

    Update Dropdown.jsx

--- epsilon/frontend/src/Dropdown.jsx
+++ epsilon/frontend/src/Dropdown.jsx
@@ -3,26 +3,19 @@ import Select, { selectClasses } from '@mui/joy/Select';
 import Option from '@mui/joy/Option';
 import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
 
-export default function Dropdown() {
-    return (
+export default function DropDown(props) {
+  const {onChange} = props;
+  return (
     <Select
-      placeholder="Select a Membership Tier..."
+      onChange={onChange}
+      placeholder={"Select subscription level"}
       indicator={<KeyboardArrowDown />}
-      sx={{
-        
-        [`& .${selectClasses.indicator}`]: {
-          transition: '0.2s',
-          [`&.${selectClasses.expanded}`]: {
-            transform: 'rotate(-180deg)',
-          },
-        },
-      }}
+      
     >
       <Option value="None">None</Option>
       <Option value="Level1">Level1</Option>
       <Option value="Level2">Level2</Option>
       <Option value="Level3">Level3</Option>
-      
     </Select>
   );
 }
