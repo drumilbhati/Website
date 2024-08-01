4b1fd1a5686a1c8e8a41b8318ce7bfe4cf034d2f
drumilbhati <drumilbhati5@gmail.com>
2024-08-01 09:44:45 +0530

    chore: Update frontend proxy URL to use HTTPS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:3001'
+      '/api': 'https://website-8t82.onrender.com'
     }
   },
   plugins: [react()],
